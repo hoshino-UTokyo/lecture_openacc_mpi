8c12bd1046fbb2ca924d29b017850001c0a4654a
hoshino-UTokyo <hoshino@cc.u-tokyo.ac.jp>
2021-07-15 17:18:34 +0900

    fix run.sh

--- C/openacc_mpi_gpudirect_bench/06_managed_internode/run.sh
+++ C/openacc_mpi_gpudirect_bench/06_managed_internode/run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-#PJM -L rscgrp=lecture-a
-#PJM -L gpu=2
+#PJM -L rscgrp=short-a
+#PJM -L node=2
+#PJM --mpi proc=2
 #PJM -L elapse=00:10:00
-#PJM -g gt00
+#PJM -g gx44
 
 
 
@@ -10,7 +11,4 @@ module load nvidia cuda ompi-cuda
 
 export UCX_MEMTYPE_CACHE=no
 
-mpirun -np 2 ./run
-
-
-
+mpiexec -machinefile $PJM_O_NODEINF -n $PJM_MPI_PROC -npernode 1 ./run
